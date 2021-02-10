88e3c24444bfb687d2ea3421092cc13903106d26
Michael Reed <mreed5812@gmail.com>
2021-02-09 22:29:24 -0500

    added price change

--- app.py
+++ app.py
@@ -15,14 +15,16 @@ response = requests.get(main_url)
 soup = BeautifulSoup(response.text, "html.parser")
 
 price = soup.find("span", {"class": "Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)"}).text
+price_change = soup.find("span", {"class": "Trsdu(0.3s) Fw(500) Pstart(10px) Fz(24px) C($negativeColor)"}).text
 
 price = float(price)
 print(price)
+print(price_change)
 
 if price < 70:
     message = client.messages \
                 .create(
-                     body="Price is less than $70",
+                     body="Today's closing price is {price}, and the change is {price_change}",
                      from_='+16072142415',
                      to='+16304181900'
                 )
