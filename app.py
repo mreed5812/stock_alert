517a5660e1cb793a02e05eaacd1c27270e4f5156
Michael Reed <mreed5812@gmail.com>
2021-03-16 21:49:20 -0400

    stripped price change percent from price change number

--- app.py
+++ app.py
@@ -17,11 +17,22 @@ soup = BeautifulSoup(response.text, "html.parser")
 price = soup.find("span", {"class": "Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)"}).text
 price_change = soup.find("span", {"class": "Trsdu(0.3s) Fw(500) Pstart(10px) Fz(24px) C($negativeColor)"}).text
 
+pc = price_change.split(' ')[1]
+
+new_pc = pc[pc.find("(")+1:pc.find(")")]
+
+
+def p2f(x):
+     return float(x.strip('%'))/100
+
+print(p2f(new_pc))
 price = float(price)
 print(price)
 print(price_change)
+print(new_pc)
+price_change_alert = .05
 
-if price < 70:
+if float(price) < price_change_alert:
     message = client.messages \
                 .create(
                      body="Today's closing price is {price}, and the change is {price_change}",
